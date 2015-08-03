4b9af8371b577eba69a9fef9a58f4e78413d445d
Shyam Guthikonda <shyamguth@gmail.com>
2015-08-02 17:40:14 -0700

    Add nickname functionality + generic rpc handling.

--- index.js
+++ index.js
@@ -1,17 +1,94 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var _ = require('lodash');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function(socket) {
+    onConnect(socket);
+
+    socket.on('disconnect', function() {
+        return onDisconnect(socket);
+    });
+
+    /**
+     * Forward the packet to all connected clients.
+     */
+    socket.on('send_all', function(data) {
+        io.emit('rpc', data);
+    });
+    /**
+     * Forward the packet to all connected clients EXCEPT the sender.
+     */
+    socket.on('broadcast', function(data) {
+        socket.broadcast.emit('rpc', data);
+    });
+    /**
+     * Forward the packet to the specified client.
+     */
+    socket.on('send', function(data) {
+        var socketId = data.socketId;
+        if (socketId) {
+            io.to(socketId).emit('rpc', data);
+        }
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+function onConnect(socket) {
+    var clients = getConnectedClientSockets();
+    console.log('client connected: ' + socket.id + ' (total: ' + clients.length + ')');
+
+    // Alert other clients.
+    socket.broadcast.emit('client_connected', {
+        socketId: socket.id
+    });
+
+    // Alert this client of the other clients already existing.
+    _.forEach(clients, function(client) {
+        if (client.id !== socket.id) {
+            socket.emit('client_connected', {
+                socketId: client.id
+            });
+        }
+    });
+}
+
+function onDisconnect(socket) {
+    var clients = getConnectedClientSockets();
+    console.log('client disconnected: ' + socket.id + ' (total: ' + clients.length + ')');
+
+    // Alert other clients.
+    socket.broadcast.emit('client_disconnected', {
+        socketId: socket.id
+    });
+}
+
+/**
+ * Returns a list of the connected clients sockets.
+ * See: http://stackoverflow.com/questions/6563885/socket-io-how-do-i-get-a-list-of-connected-sockets-clients
+ */
+function getConnectedClientSockets(roomId, namespace) {
+    var res = [],
+        ns = io.of(namespace || "/"); // the default namespace is "/"
+
+    if (ns) {
+        for (var id in ns.connected) {
+            if (roomId) {
+                var index = ns.connected[id].rooms.indexOf(roomId);
+                if (index !== -1) {
+                    res.push(ns.connected[id]);
+                }
+            } else {
+                res.push(ns.connected[id]);
+            }
+        }
+    }
+    return res;
+}
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
